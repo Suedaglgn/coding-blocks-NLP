50b0434bb437d762c17edb55291e4e90c79ed980
Suedaglgn <sueda.gulgun@istanbulticaret.edu.tr>
2022-04-19 12:24:50 +0300

    Sample outputs added

--- normalizer.py
+++ normalizer.py
@@ -35,3 +35,15 @@ def rm_irrelevant_chars(text):
                   text).replace("\n", " ")
     return text
 
+
+if __name__ == '__main__':
+    a = asciify("Bugün güneşli bir gün.")
+    print(a)
+    # output:   Bugun gunesli bir gun.
+    d = deasciify("Bugun guneslı bır gun.")
+    print(d)
+    # output:   Bugün güneşli bir gün.
+    rm = rm_irrelevant_chars("Bu%g+ün ^gu'neş>li |b?ir g£#ün")
+    print(rm)
+    # output:   Bugün guneşli bir gün
+    
